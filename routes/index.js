c5710b0251b5e8ff662634ba170b9fa14f658444
Glitch (christophermarek-mini-message-board) <none>
2020-04-03 03:23:44 +0000

    Project completed, minus styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [ 
+  { text: "Hi there!", user: "Amando", added: new Date() }, 
+  { text: "Hello World!", user: "Charles", added: new Date() }
+];
+
+router.post('/new', function (req, res) {
+  var name = req.body.AuthorsName
+  var message = req.body.Message
+  messages.push({text: message, user: name, added: new Date()});
+  res.redirect('/');
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
